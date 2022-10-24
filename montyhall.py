977389c0e4486a17544a188490a0f35273894498
Megan Morris <Megan17morris@gmail.com>
2022-10-24 16:02:14 -0400

    montypy comments

--- montyhall.py
+++ montyhall.py
@@ -1,1 +1,10 @@
 #This is just a run once simulator for monty hall, to create a function
+#opt. set doors
+#1. Randomize car for 1 through 3.
+#2. Prompt user to pick a door
+#3. If goat door, open other goat door. If not goat door, open random other door
+#4. Prompt to stay or switch.
+#5 Reveal
+
+
+#separate function to this randomly many times, sticking with stay or switch option
